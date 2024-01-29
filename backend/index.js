105b30ed130182af9db3d6e598bd0bc1e2350c8d
Praneeth <praneethayla@gmail.com>
2024-01-29 21:09:28 +0530

    connected backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types.js");
 const { todo } = require("./db.js");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/todo", async function (req, res) {
     const createPayload = req.body;
